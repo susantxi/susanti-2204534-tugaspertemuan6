a76931a1865594de15a68ca23654be7f31988941
susanti <144995592+susantxi@users.noreply.github.com>
2024-03-09 12:14:23 +0700

    no_1.java

--- no_1.java
+++ no_1.java
@@ -3,10 +3,10 @@ package java_method;
 import java.util.Scanner;
 
 public class no_1 {  
-    static void cekRibuan(int i, int bil){
-        if(bil >= 1000 && bil <= 9999){
+    static void cekRibuan (int i, int bil) {
+        if (bil >= 1000 && bil <= 9999) {
             System.out.println("Bilangan ke-" + (i+1) + " merupakan bilangan ribuan");
-        }else{
+        } else {
             System.out.println("Bilangan ke-" + (i+1) + " bukan bilangan ribuan");
         }
     }
@@ -15,14 +15,14 @@ public class no_1 {
         Scanner userInput = new Scanner(System.in);
         
         int[] bilangan = new int[3];
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             System.out.printf("Masukkan bilangan ke-%d: ", i+1);
             bilangan[i] = userInput.nextInt();
         }
         
         System.out.println();
         
-        for(int i = 0; i  < 3; i++){
+        for(int i = 0; i  < 3; i++) {
             cekRibuan(i, bilangan[i]);
         }
         userInput.close();
