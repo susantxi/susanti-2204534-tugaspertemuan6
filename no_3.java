f2edc0d5aaaf6d2d4a466ecc7cbecfb2dbb6d05f
susanti <144995592+susantxi@users.noreply.github.com>
2024-03-09 12:15:20 +0700

    no_3.java

--- no_3.java
+++ no_3.java
@@ -3,9 +3,9 @@ package java_method;
 import java.util.Scanner;
 
 public class no_3 {
-    static void polaBintang(int N){//jumlah baris -> N
-            for(int i = 1 ; i <= N; i++){//baris
-                for(int j = 1; j <= i; j++){//jumlah bintang
+    static void polaBintang (int N) {//jumlah baris -> N
+            for (int i = 1 ; i <= N; i++) {//baris
+                for (int j = 1; j <= i; j++) {//jumlah bintang
                     System.out.print("*");
                 }
                 System.out.println();
@@ -18,9 +18,9 @@ public class no_3 {
         System.out.print("Masukkan nilai : ");
         int N = userInput.nextInt();
         
-        if(N > 0 && N <= 1000){
+        if (N > 0 && N <= 1000) {
             polaBintang(N);
-        }else{
+        } else {
             System.out.println("Tidak Ada Hasil Untuk Nilai " + N);
         }
         userInput.close();
